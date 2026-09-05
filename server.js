a097616a5c368b607a3a1613b570549df30e5119
anhtuanby77-debug <anhtuanby77@gmail.com>
2026-09-05 22:09:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '8782950405'
+const token = '8634508240:AAGJ0-pdHAH0qi41apBAxtuy4dvT1aq3VRI'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
